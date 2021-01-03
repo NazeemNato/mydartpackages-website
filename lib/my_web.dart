858b081462dc8e647a31c21cec3f7521785b654b
nazeemnato <nazeemnob18@gmail.com>
2021-01-03 08:26:14 +0530

    website lib folder

---
+++ lib/my_web.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyWeb extends StatelessWidget {
+  const MyWeb({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My packages',
+      debugShowCheckedModeBanner: false,
+      // will do it later
+    );
+  }
+}
